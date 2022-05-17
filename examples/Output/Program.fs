ca773297f1870c41dbe0e187d0f5f502ca47d308
Max Paige <max.paige@binarydefense.com>
2022-05-17 13:23:54 -0400

    Add example project

---
+++ examples/Output/Program.fs
@@ -0,0 +1,17 @@
+﻿open Wraith
+open Wraith.Ansi
+open Wraith.Ansi.Operators
+
+//let name = Console.prompt (!! "Enter name: ")
+
+// let msg =
+//     !! $"Hello world from F#"
+//     |> Message.withColor StandardColor.Red
+
+// Console.writeLine msg
+
+open System
+let private width = System.Console.BufferWidth
+let private height = System.Console.BufferHeight
+
+printfn $"height x width: %i{height} x %i{width}"
