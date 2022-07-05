6915b33499bec4c197cdd3b9c03b6cbca3d90e5d
Max Paige <max.paige@binarydefense.com>
2022-07-05 09:35:22 -0400

    Bump version to 0.1.0
    ## [0.1.0] - 2022-07-05
    
    ### Added
    - Text prompt
    - Int prompt
    - List prompt
    - Numbered list prompt
    - Paging on list prompts

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,10 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
-## [0.1.0] - 2017-03-17
-First release
-
+## [0.1.0] - 2022-07-05
 ### Added
-- This release already has lots of features
-
-[0.1.0]: https://github.com/user/MyCoolNewLib.git/releases/tag/v0.1.0
+- Text prompt
+- Int prompt
+- List prompt
+- Numbered list prompt
+- Paging on list prompts
--- src/Wraith/AssemblyInfo.fs
+++ src/Wraith/AssemblyInfo.fs
@@ -5,19 +5,19 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Wraith")>]
 [<assembly: AssemblyProductAttribute("Wraith")>]
 [<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2017-03-17T00:00:00.0000000")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2022-07-05T00:00:00.0000000-04:00")>]
 [<assembly: AssemblyFileVersionAttribute("0.1.0")>]
 [<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","bb8964b54bee133e9af64d316dc2cfee16df7f72")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","ffa5869a85db9f4bfd0792dd73dbca4f7bdb6f1c")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Wraith"
     let [<Literal>] AssemblyProduct = "Wraith"
     let [<Literal>] AssemblyVersion = "0.1.0"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2017-03-17T00:00:00.0000000"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2022-07-05T00:00:00.0000000-04:00"
     let [<Literal>] AssemblyFileVersion = "0.1.0"
     let [<Literal>] AssemblyInformationalVersion = "0.1.0"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "bb8964b54bee133e9af64d316dc2cfee16df7f72"
+    let [<Literal>] AssemblyMetadata_GitHash = "ffa5869a85db9f4bfd0792dd73dbca4f7bdb6f1c"
